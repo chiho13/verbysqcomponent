20647dcd51bf930500f7a5028960a5f3e7effdce
chiho13 <ahomnky11@gmail.com>
2023-03-05 01:41:57 +0000

    refactor button

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./index.css";
 import SampleAudioVoice from "./components/SampleAudioVoice";
 import AudioPlayer from "./components/AudioPlayer";
 import VoiceDropdown from "./components/VoiceDropdown";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   const [selectedItemText, setSelectedItemText] =
@@ -37,7 +38,7 @@ function App() {
       if (transcriptionId) {
         try {
           const response = await fetch(
-            `http://localhost:3000/articleStatus/${transcriptionId}`
+            `https://verbyttsapi.vercel.app/articleStatus/${transcriptionId}`
           );
           if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
@@ -168,32 +169,8 @@ function App() {
         >
           {audioIsLoading ? (
             <>
-              <svg
-                aria-hidden="true"
-                role="status"
-                class="spinning-icon inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-gray-600"
-                viewBox="0 0 100 100"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill="#aaaaaa"
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
-                >
-                  <animateTransform
-                    attributeName="transform"
-                    attributeType="XML"
-                    type="rotate"
-                    dur="1s"
-                    from="0 50 50"
-                    to="360 50 50"
-                    repeatCount="indefinite"
-                  />
-                </path>
-              </svg>
-              <div>Loading...</div>
+              <LoadingSpinner />
+              <div>Generating...</div>
             </>
           ) : (
             <div>Generate</div>
